3ee3cb9fcb2386290ff5fe74a4182e9813fe38e2
AshokReddy01 <kummethashokumareddy@gmail.com>
2019-11-26 22:48:41 +0530

    Double LinkedList using Python

--- DoubleLinkedList.py
+++ DoubleLinkedList.py
@@ -1,4 +1,4 @@
-from pip._vendor.requests.api import head
+import time
 
 class Node:
     length=0
@@ -34,7 +34,7 @@ class Node:
     def hasNext(self):
         return self.nextNode()!=None  
     
-    def insert_value_at_starting(self,value):
+    def insertValueAtStarting(self,value):
         current=self.head
         
         newNode=Node()
@@ -51,21 +51,77 @@ class Node:
          
     def Display(self):
         currentNode=self.head
+       
         print("From Starting to end : ",currentNode.get_data() ,end=" ")
         while currentNode.get_nextnode()!=None:
             currentNode=currentNode.get_nextnode()
             print(currentNode.get_data(),end=" ")
         print()
+  
         print("From back to front : ",currentNode.get_data() ,end=" ")
         while currentNode.get_pre_node()!=None:
             currentNode=currentNode.get_pre_node()
             print(currentNode.get_data(),end=" ")
+    
+    def insertValueInLast(self,value):
+        currentNode=self.head
+        while currentNode.get_nextnode()!=None:
+            currentNode=currentNode.get_nextnode()
+        newNode=Node()
+        newNode.data=value
+        newNode.set_nextnode(None)
+        newNode.set_pre_node(currentNode)
+        currentNode.set_nextnode(newNode)
+        self.length+=1
         
+    def addValueAfter(self,data,value):
+        currentNode=self.head
+        prevNode=None
+        while currentNode.get_data()!= value:
+            prevNode=currentNode
+            currentNode=currentNode.get_nextnode()
+        prevNode=currentNode
+        currentNode=currentNode.get_nextnode()
+        
+        newNode=Node()
+        newNode.set_data(data)
+        newNode.set_nextnode(currentNode)
+        newNode.set_pre_node(prevNode)
+        currentNode.set_pre_node(newNode)
+        prevNode.set_nextnode(newNode)
+        self.length+=1
+        
+    def deleteAtStarting(self):
+        currentNode=self.head
+        self.head=currentNode.get_nextnode()
+        self.head.set_pre_node(None)  
+        
+     
+# ''''''    def inserValueAtEnd(self,value):
+#         currentNode=self.head
+#         while currentNode.get_nextnode()!=None :
+#             currentNode=currentNode.get_nextnode()
+#         newNode=Node() 
+#         newNode.set_data(value)
+#         newNode.set_pre_node(currentNode)
+#         newNode.get_nextnode(None)
+#         currentNode.get_nextnode(newNode)
+#         self.length+=1'''
+et=time.time()   
 head=Node()
-head.insert_value_at_starting(21)
-head.insert_value_at_starting(22)
+head.insertValueAtStarting(21)
+head.insertValueAtStarting(22)
 
-head.insert_value_at_starting(24)
+head.insertValueAtStarting(24)
 
-head.insert_value_at_starting(23)
+head.insertValueAtStarting(23)
+
+head.insertValueInLast(100)
+
+head.addValueAfter(215, 22)
+
+head.deleteAtStarting()
+
 head.Display()  
+print()
+print(time.time()-et)
